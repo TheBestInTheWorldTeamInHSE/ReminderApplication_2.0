b5622f6be28eade165095b9b251572d533f8793e
Aleksa <aleksa.khruleva@gmail.com>
2020-07-20 19:40:25 +0300

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Aleksa on 20.07.2020.
+//  Copyright © 2020 Aleksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
